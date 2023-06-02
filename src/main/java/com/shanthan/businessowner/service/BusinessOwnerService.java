298f3dfc3542db15461c13e5215a9c50536f1b16
Shanthan Cheruku <shanthanc@gmail.com>
2023-06-02 14:01:38 -0500

    new features BusinessOwnerService.java and BusinessOwnerServiceTest.java added

--- src/main/java/com/shanthan/businessowner/service/BusinessOwnerService.java
+++ src/main/java/com/shanthan/businessowner/service/BusinessOwnerService.java
@@ -1,10 +1,16 @@
 package com.shanthan.businessowner.service;
 
-import com.shanthan.businessowner.controller.BusinessOwnerController;
+import com.shanthan.businessowner.exception.BusinessOwnerException;
 import com.shanthan.businessowner.model.BusinessOwner;
+import com.shanthan.businessowner.repository.BusinessOwnerEntity;
 import com.shanthan.businessowner.repository.BusinessOwnerRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Service
 @Slf4j
@@ -12,11 +18,47 @@ public class BusinessOwnerService {
 
     private final BusinessOwnerRepository businessOwnerRepository;
 
-    public BusinessOwnerService(BusinessOwnerRepository businessOwnerRepository) {
+    private final MapperService mapperService;
+
+    public BusinessOwnerService(BusinessOwnerRepository businessOwnerRepository,
+                                MapperService mapperService) {
         this.businessOwnerRepository = businessOwnerRepository;
+        this.mapperService = mapperService;
     }
 
-    public BusinessOwner getBusinessOwnerByBusinessIdFromDb(Long businessId) {
-        return null;
+    public BusinessOwner getBusinessOwnerByBusinessIdFromDb(Long businessId) throws BusinessOwnerException {
+        BusinessOwner businessOwner;
+        try {
+            log.info("Retrieving BusinessOwner with Id {} from Db ", businessId);
+            BusinessOwnerEntity businessOwnerEntity =
+                    businessOwnerRepository.getBusinessOwnerEntityByBusinessId(businessId);
+            businessOwner = mapperService.mapEntityToObject(businessOwnerEntity);
+        } catch (Exception ex) {
+            log.error("Error occurred while retrieving BusinessOwner details from Db ");
+            throw new BusinessOwnerException(INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
+        }
+        return businessOwner;
+
     }
+
+    public List<BusinessOwner> getBusinessOwnerListByFirstName(String firstName) throws BusinessOwnerException {
+        if (firstName.isBlank()) {
+            log.error("First name is null or empty. ");
+            throw new BusinessOwnerException(INTERNAL_SERVER_ERROR, "First name is null or empty. Cannot proceed.");
+        }
+        List<BusinessOwner> businessOwners;
+        try {
+            List<BusinessOwnerEntity> entityList =
+                    businessOwnerRepository.getBusinessOwnerEntitiesByFirstNameOrderByFirstName(firstName);
+            businessOwners = new ArrayList<>();
+            for (BusinessOwnerEntity entity : entityList) {
+                businessOwners.add(mapperService.mapEntityToObject(entity));
+            }
+        } catch (Exception e) {
+            log.error("Error while retrieving list of business owner by first name ");
+            throw new BusinessOwnerException(INTERNAL_SERVER_ERROR, e.getMessage(), e);
+        }
+        return businessOwners;
+    }
+
 }
